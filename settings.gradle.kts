d35f34166c99610e2dc599d19eb4ab48fd06ba50
filloax <filloax@gmail.com>
2024-10-13 00:25:42 +0200

    Revert "switch to minivan for vanillagradle"
    This reverts commit 9299d595742918cf9856e1ba3c4fdb1d08107708.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,7 +29,6 @@ pluginManagement {
                 includeGroupAndSubgroups("org.parchmentmc")
             }
         }
-        maven { url = uri("https://repo.sleeping.town/") }
         maven {
             name = "Sponge"
             url = uri("https://repo.spongepowered.org/repository/maven-public/")
