31cd0b74097f8af14c4502f158d09bcf440c8957
filloax <filloax@gmail.com>
2024-10-13 16:09:29 +0200

    add networking API

---
+++ fabric/src/main/kotlin/com/filloax/fxlib/networking/FxLibNetworkingFabric.kt
@@ -0,0 +1,28 @@
+package com.filloax.fxlib.networking
+
+import com.filloax.fxlib.api.networking.FxLibNetworking
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking
+import net.minecraft.network.PacketSendListener
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload
+import net.minecraft.server.level.ServerPlayer
+
+class FxLibNetworkingFabric : FxLibNetworking {
+    override fun <T : CustomPacketPayload> sendPacketToPlayer(
+        player: ServerPlayer,
+        payload: T,
+        callback: PacketSendListener?
+    ) {
+        ServerPlayNetworking.getSender(player).sendPacket(payload, callback)
+    }
+
+    override fun <T : CustomPacketPayload> sendPacketToServer(payload: T, callback: PacketSendListener?) {
+        try {
+            ClientPlayNetworking.send(payload)
+            callback?.onSuccess()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            callback?.onFailure()
+        }
+    }
+}
