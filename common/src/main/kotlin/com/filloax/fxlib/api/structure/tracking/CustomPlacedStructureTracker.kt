674928fb8788a731ec41fc23a33d51d241ca1416
costantin0 <86307814+costantin0@users.noreply.github.com>
2026-01-05 15:11:43 +0100

    1.21.11: fixed compile errors in CustomPlacedStructureTracker, must be actually updated to use SavedDataTypes
    (some changes are unrelated to it and must stay)

--- common/src/main/kotlin/com/filloax/fxlib/api/structure/tracking/CustomPlacedStructureTracker.kt
+++ common/src/main/kotlin/com/filloax/fxlib/api/structure/tracking/CustomPlacedStructureTracker.kt
@@ -3,6 +3,8 @@ package com.filloax.fxlib.api.structure.tracking
 import com.filloax.fxlib.FxLib
 import com.filloax.fxlib.api.chunk.boundBoxChunkRange
 import com.filloax.fxlib.api.nbt.getCompoundOrNull
+import com.mojang.serialization.Codec
+import com.mojang.serialization.codecs.RecordCodecBuilder
 import it.unimi.dsi.fastutil.longs.LongOpenHashSet
 import it.unimi.dsi.fastutil.longs.LongSet
 import net.minecraft.core.BlockPos
@@ -18,13 +20,26 @@ import net.minecraft.world.level.levelgen.structure.Structure
 import net.minecraft.world.level.levelgen.structure.StructureStart
 import net.minecraft.world.level.levelgen.structure.pieces.StructurePieceSerializationContext
 import net.minecraft.world.level.saveddata.SavedData
+import net.minecraft.world.level.saveddata.SavedDataType
 import java.lang.IllegalStateException
+
+// TODO: CLASS EDITED JUST FOR COMPILING, ORIGINAL CODE IS IN ////, MUST BE ACTUALLY FIXED BY USING SavedDataType correctly
 
 /**
  * Allows manually tracking structures spawned outside of normal world gen,
  * and uses mixin to make them interact with /locate, achievements, etc.
  */
-class CustomPlacedStructureTracker private constructor(val level: ServerLevel) : SavedData() {
+////class CustomPlacedStructureTracker private constructor(val level: ServerLevel) : SavedData() {
+
+//justforcompile
+class CustomPlacedStructureTracker() : SavedData() {
+    var level: ServerLevel? = null
+
+    constructor(a: Long) : this() {
+        lastReference = a
+    }
+//endjustforcompile
+
     val structureData = mutableMapOf<Long, PlacedStructureData>()
 
     // chunk positions (long) of starting chunks of the structures that pass from each chunk key, as in base game chunk structureRefs
@@ -41,37 +56,60 @@ class CustomPlacedStructureTracker private constructor(val level: ServerLevel) :
     private var lastReference = 0L
 
     companion object {
-        private val factory = { level: ServerLevel ->
-            Factory(
-                { CustomPlacedStructureTracker(level) }, { tag, _ -> load(tag, level) },
-                DataFixTypes.STRUCTURE
-            )
+////        private val factory = { level: ServerLevel ->
+////            Factory(
+////                { CustomPlacedStructureTracker(level) }, { tag, _ -> load(tag, level) },
+////                DataFixTypes.STRUCTURE
+////            )
+////        }
+
+//justforcompile
+        val CODEC: Codec<CustomPlacedStructureTracker> = RecordCodecBuilder.create {
+            instance -> instance.group(
+                Codec.LONG.fieldOf("s").forGetter(CustomPlacedStructureTracker::lastReference)
+            ).apply(instance, ::CustomPlacedStructureTracker)
         }
+
+        val TYPE = SavedDataType<CustomPlacedStructureTracker>(
+            "fxlib_structure_placement_tracking",
+            ::CustomPlacedStructureTracker,
+            CODEC,
+            DataFixTypes.STRUCTURE
+        )
+//endjustforcompile
 
         @JvmStatic
         fun get(level: ServerLevel): CustomPlacedStructureTracker {
-            return level.dataStorage.computeIfAbsent(factory(level), "fxlib_structure_placement_tracking")
+            ///return level.dataStorage.computeIfAbsent(factory(level), "fxlib_structure_placement_tracking")
+//justforcompile
+            return level.dataStorage.computeIfAbsent(TYPE)
+//endjustforcompile
         }
 
         private fun load(compoundTag: CompoundTag, level: ServerLevel): CustomPlacedStructureTracker {
-            val out = CustomPlacedStructureTracker(level)
+            ////val out = CustomPlacedStructureTracker(level)
+//justforcompile
+            val out = CustomPlacedStructureTracker()
+            out.level = level
+//endjustforcompile
             val ctx = StructurePieceSerializationContext.fromLevel(level)
             compoundTag.getCompoundOrNull("spawnedStructureData")?.let { tag ->
-                tag.allKeys.forEach { idStr ->
+                tag.keySet().forEach { idStr ->
                     val id = idStr.toLong()
-                    val data = PlacedStructureData.load(tag.getCompound(idStr), ctx, level)
+                    val data = PlacedStructureData.load(tag.getCompound(idStr).get(), ctx, level)
                     out.structureData[id] = data
 
                     out.cacheData(data, id)
                 }
             }
-            out.lastReference = compoundTag.getLong("references")
+            out.lastReference = compoundTag.getLong("references").get()
             return out
         }
     }
 
-    override fun save(compoundTag: CompoundTag, holderLookup: HolderLookup.Provider): CompoundTag {
-        val ctx = StructurePieceSerializationContext.fromLevel(level)
+    //// was override
+    fun save(compoundTag: CompoundTag, holderLookup: HolderLookup.Provider): CompoundTag {
+        val ctx = StructurePieceSerializationContext.fromLevel(level!!)
         compoundTag.put("spawnedStructureData", CompoundTag().also { tag ->
             structureData.forEach { (refId, data) ->
                 tag.put(refId.toString(), data.save(ctx))
@@ -105,7 +143,7 @@ class CustomPlacedStructureTracker private constructor(val level: ServerLevel) :
     }
 
     fun getByStructure(key: ResourceKey<Structure>): List<PlacedStructureData> {
-        val structure = level.registryAccess().lookupOrThrow(Registries.STRUCTURE).getValueOrThrow(key)
+        val structure = level!!.registryAccess().lookupOrThrow(Registries.STRUCTURE).getValueOrThrow(key)
         return byStructure[structure] ?: listOf()
     }
 
@@ -174,7 +212,8 @@ data class PlacedStructureData(
     companion object {
         fun load(tag: CompoundTag, ctx: StructurePieceSerializationContext, level: ServerLevel): PlacedStructureData {
             return PlacedStructureData(
-                StructureStart.loadStaticStart(ctx, tag.getCompound("StructureStart"), level.seed) ?: throw IllegalStateException("Couldn't load structure start from $tag"),
+                StructureStart.loadStaticStart(ctx, tag.getCompound("StructureStart").get(), level.seed)
+                    ?: throw IllegalStateException("Couldn't load structure start from $tag"),
                 BlockPos.CODEC.decode(NbtOps.INSTANCE, tag.get("BlockPos")).getOrThrow {
                     Exception("Error in decoding BlockPos: $it")
                 }.first,
