e3cd0b59006ee2b65ffba3cf175fb40c53ca39b0
filloax <filloax@gmail.com>
2026-06-30 10:12:14 +0200

    fix structure generation track maybe

--- common/src/main/java/com/filloax/fxlib/mixin/structuretrack/ChunkGeneratorTrackMixin.java
+++ common/src/main/java/com/filloax/fxlib/mixin/structuretrack/ChunkGeneratorTrackMixin.java
@@ -30,17 +30,13 @@ import java.util.Set;
 import java.util.stream.Stream;
 
 @Mixin(ChunkGenerator.class)
-// Currently DISABLED as this led to more issues than it was worth, meaning fixedstructures won't be findable with /locate and similar
-// (most usecases will use it directly anyways)
 public abstract class ChunkGeneratorTrackMixin {
     @WrapOperation(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/chunk/ChunkGeneratorStructureState;getPlacementsForStructure(Lnet/minecraft/core/Holder;)Ljava/util/List;"),
             method = "findNearestMapStructure")
     private List<StructurePlacement> findNearestMapStructureChangePlacement(ChunkGeneratorStructureState instance, Holder<Structure> structure, Operation<List<StructurePlacement>> original, ServerLevel level) {
         List<StructurePlacement> basePlacements = original.call(instance, structure);
-        Stream<StructurePlacement> fixedPlacements = structure.unwrap().map(
-                key -> CustomPlacedStructureTracker.get(level).getByStructure(key),
-                struct -> CustomPlacedStructureTracker.get(level).getByStructure(struct)
-            ).stream().map(PlacedStructureData::getPlacement);
+        Stream<StructurePlacement> fixedPlacements = CustomPlacedStructureTracker.get(level)
+                .getByStructure(structure.value()).stream().map(PlacedStructureData::getPlacement);
 
         return Stream.concat(basePlacements.stream(), fixedPlacements).toList();
     }
